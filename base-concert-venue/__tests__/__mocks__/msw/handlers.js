e617ea97d2bc8b79600538d7d3e8786b41ca71b3
Laurie Roy <laurieroy.dev@gmail.com>
2022-12-30 09:35:09 -0800

    Update handler to handle show Id, Test case no tix avail

--- base-concert-venue/__tests__/__mocks__/msw/handlers.js
+++ base-concert-venue/__tests__/__mocks__/msw/handlers.js
@@ -6,7 +6,10 @@ import { fakeUserReservations } from "../fakeData/userReservations";
 export const handlers = [
   rest.get("http://localhost:3000/api/shows/:showId", async (req, res, ctx) => {
     const { fakeShows } = await readFakeData();
-    return res(ctx.json({ show: fakeShows[0] }));
+    const { showId } = req.params;
+    // showId index = 0 has seats available in fake data
+    // showId index = 1 has NO seats available in fake data
+    return res(ctx.json({ show: fakeShows[Number(showId)] }));
   }),
   rest.get(
     "http://localhost:3000/api/users/:userId/reservations",
--- base-concert-venue/__tests__/ui/reservation.test.tsx
+++ base-concert-venue/__tests__/ui/reservation.test.tsx
@@ -8,3 +8,14 @@ it("reservation page shows correct number of seats available", async () => {
   const seatCountText = await screen.findByText(/10 seats left/i);
   expect(seatCountText).toBeInTheDocument();
 });
+test("reservation page shows 'sold out' messange and no purchase button if there are no seats available", async () => {
+  render(<Reservation showId={1} submitPurchase={jest.fn()} />);
+
+  const soldOutMessage = await screen.findByRole("heading", {
+    name: /sold out/i,
+  });
+  expect(soldOutMessage).toBeInTheDocument();
+
+  const purchaseButton = screen.queryByRole("button", { name: /purchase/i });
+  expect(purchaseButton).not.toBeInTheDocument();
+});
