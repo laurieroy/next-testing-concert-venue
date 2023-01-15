9ca802e77d0ef656c75a9de55ca79041b27e274f
Laurie Roy <laurieroy.dev@gmail.com>
2023-01-14 20:05:54 -0800

    Create test dbs + test env + method to reset db

---
+++ __tests__/__mocks__/db/utils/reset-db.ts
@@ -0,0 +1,21 @@
+import { readFakeData } from "../../fakeData";
+import { filenames, writeJSONToFile } from "@/lib/db/db-utils";
+
+export const resetDb = async () => {
+  // failsafe against resetting prod db
+  const safeToReset = process.env.NODE_ENV === "test";
+  if (!safeToReset) {
+    console.log("WARNING: database reset unavailable outside test environment!");
+  
+  return;
+  }
+  const { fakeShows, fakeBands, fakeUsers, fakeReservations } = await readFakeData();
+
+  // overwrite fake data
+  await Promise.all([
+    writeJSONToFile(filenames.bands, fakeBands),
+    writeJSONToFile(filenames.shows, fakeShows),
+    writeJSONToFile(filenames.reservations, fakeReservations),
+    writeJSONToFile(filenames.users, fakeUsers)
+  ])
+}
